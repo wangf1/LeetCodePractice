cfc0a6c12d9bee20a2ba4b420c64dd9ffb5a497d
wangf1 <armstrong.wang2000@gmail.com>
2022-08-22 20:44:55 +0800

    142. Linked List Cycle II

---
+++ lib/src/main/java/leetcodepractice/LC142.java
@@ -0,0 +1,70 @@
+package leetcodepractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/linked-list-cycle-ii/">142. Linked List Cycle II</a>
+ */
+public class LC142 {
+    /**
+     * Hash table approach
+     *
+     * @param head list head
+     * @return start point of loop
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        Map<ListNode, Integer> nodeIndexMap = new HashMap<>();
+        int pos = 0;
+        while (head.next != null) {
+            if (nodeIndexMap.containsKey(head)) {
+                return head;
+            }
+            nodeIndexMap.put(head, pos);
+            pos++;
+            head = head.next;
+        }
+        return null;
+    }
+
+    /**
+     * See <a href="https://www.tutorialcup.com/leetcode-solutions/linked-list-cycle-ii-leetcode-solution.htm">Explanation</a>
+     * 2(x+y) - (x+y) = Nc <br>
+     * ===> x+y=Nc  <br>
+     * ===> meetPoint + y steps = the start point of cycle
+     */
+
+    public ListNode detectCycle2PointerApproach(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode meetPoint = meetPoint(head);
+        if (meetPoint == null) {
+            return null;
+        }
+
+        while (head != meetPoint) {
+            head = head.next;
+            meetPoint = meetPoint.next;
+        }
+        return head;
+
+    }
+
+    private ListNode meetPoint(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return slow;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ lib/src/test/java/leetcodepractice/LC142Test.java
@@ -0,0 +1,43 @@
+package leetcodepractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LC142Test {
+
+
+    private LC142 testee;
+
+    @BeforeEach
+    void setUp() {
+        testee = new LC142();
+    }
+
+    @Test
+    void detectCycle() {
+        ListNode n1 = new ListNode(3, null);
+        ListNode n2 = new ListNode(2, null);
+        ListNode n3 = new ListNode(0, null);
+        ListNode n4 = new ListNode(-4, null);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n2;
+
+        Assertions.assertSame(n2, testee.detectCycle(n1));
+        Assertions.assertSame(n2, testee.detectCycle2PointerApproach(n1));
+
+
+        n1.next = n2;
+        n2.next = n1;
+        Assertions.assertSame(n1, testee.detectCycle(n1));
+        Assertions.assertSame(n1, testee.detectCycle2PointerApproach(n1));
+
+        n1.next = null;
+        Assertions.assertNull(testee.detectCycle(n1));
+        Assertions.assertNull(testee.detectCycle2PointerApproach(n1));
+    }
+
+}
