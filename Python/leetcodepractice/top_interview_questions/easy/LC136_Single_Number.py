5d6cf36f1cf9cba48a884a5711cf34b890c3dd5d
wangf1 <armstrong.wang2000@gmail.com>
2022-11-15 17:51:39 +0800

    136. Single Number

---
+++ Python/leetcodepractice/top_interview_questions/easy/LC136_Single_Number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/single-number/
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = nums[0]
+        for i in range(1, len(nums)):
+            n ^= nums[i]
+        return n
