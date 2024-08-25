fb3e879317ab89a36abba5ae24e7229041c8783f
Mark Wang <armstrong.wang2000@gmail.com>
2024-08-25 03:20:20 -0600

    LC36 refined by ChatGPT.

--- lib/src/main/java/leetcodepractice/LC36.java
+++ lib/src/main/java/leetcodepractice/LC36.java
@@ -1,45 +1,24 @@
 package leetcodepractice;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
 
     public static final int SUDOKU_LENGTH = 9;
 
     public boolean isValidSudoku(char[][] board) {
-        boolean valid = validateRows(board);
-        if (!valid) {
-            return valid;
-        }
-        valid = validateColumns(board);
-        if (!valid) {
-            return valid;
-        }
-        return validateSubBoxes(board);
+        return validateRows(board) && validateColumns(board) && validateSubBoxes(board);
     }
-
 
     private boolean validateRows(char[][] board) {
         for (char[] row : board) {
-            if (lineInvalid(row)) {
-                System.out.printf("Row invalid: %s%n", Arrays.toString(row));
+            if (containsDuplicate(row)) {
+                System.out.printf("Row invalid: %s%n", new String(row));
                 return false;
             }
         }
         return true;
-    }
-
-    private boolean lineInvalid(char[] line) {
-        Map<Character, Integer> count = new HashMap<>();
-        for (char c : line) {
-            if (c == '.') {
-                continue;
-            }
-            count.put(c, count.getOrDefault(c, 0) + 1);
-        }
-        return count.values().stream().anyMatch(v -> v >= 2);
     }
 
     private boolean validateColumns(char[][] board) {
@@ -48,8 +27,8 @@ class Solution {
             for (int r = 0; r < SUDOKU_LENGTH; r++) {
                 column[r] = board[r][c];
             }
-            if (lineInvalid(column)) {
-                System.out.printf("Column invalid: %s%n", Arrays.toString(column));
+            if (containsDuplicate(column)) {
+                System.out.printf("Column invalid: %s%n", new String(column));
                 return false;
             }
         }
@@ -61,19 +40,27 @@ class Solution {
             for (int j = 0; j < 3; j++) {
                 char[] cells = new char[SUDOKU_LENGTH];
                 int cellIndex = 0;
-                int rStart = i * 3;
-                int cStart = j * 3;
-                for (int r = rStart; r < rStart + 3; r++) {
-                    for (int c = cStart; c < cStart + 3; c++) {
+                for (int r = i * 3; r < i * 3 + 3; r++) {
+                    for (int c = j * 3; c < j * 3 + 3; c++) {
                         cells[cellIndex++] = board[r][c];
                     }
                 }
-                if (lineInvalid(cells)) {
-                    System.out.printf("Sub-box invalid: %s%n", Arrays.toString(cells));
+                if (containsDuplicate(cells)) {
+                    System.out.printf("Sub-box invalid: %s%n", new String(cells));
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private boolean containsDuplicate(char[] line) {
+        Set<Character> seen = new HashSet<>();
+        for (char c : line) {
+            if (c != '.' && !seen.add(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
